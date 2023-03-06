8463010b87d9b982ebd851c4dd4c0db98f44a221
olopf <luke.asgill@gmail.com>
2023-03-06 12:14:40 +1000

    feat: Implement assignment 3 part 1 systemcalls functions

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,9 @@
 #include "systemcalls.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -6,94 +11,125 @@
  *   successfully using the system() call, false if an error occurred,
  *   either in invocation of the system() call, or if a non-zero return
  *   value was returned by the command issued in @param cmd.
-*/
-bool do_system(const char *cmd)
-{
-
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
-
-    return true;
+ */
+bool do_system(const char *cmd) {
+  int returncode = system(cmd);
+  return returncode == 0;
 }
 
 /**
-* @param count -The numbers of variables passed to the function. The variables are command to execute.
-*   followed by arguments to pass to the command
-*   Since exec() does not perform path expansion, the command to execute needs
-*   to be an absolute path.
-* @param ... - A list of 1 or more arguments after the @param count argument.
-*   The first is always the full path to the command to execute with execv()
-*   The remaining arguments are a list of arguments to pass to the command in execv()
-* @return true if the command @param ... with arguments @param arguments were executed successfully
-*   using the execv() call, false if an error occurred, either in invocation of the
-*   fork, waitpid, or execv() command, or if a non-zero return value was returned
-*   by the command issued in @param arguments with the specified arguments.
-*/
+ * @param count -The numbers of variables passed to the function. The variables
+ * are command to execute. followed by arguments to pass to the command Since
+ * exec() does not perform path expansion, the command to execute needs to be an
+ * absolute path.
+ * @param ... - A list of 1 or more arguments after the @param count argument.
+ *   The first is always the full path to the command to execute with execv()
+ *   The remaining arguments are a list of arguments to pass to the command in
+ * execv()
+ * @return true if the command @param ... with arguments @param arguments were
+ * executed successfully using the execv() call, false if an error occurred,
+ * either in invocation of the fork, waitpid, or execv() command, or if a
+ * non-zero return value was returned by the command issued in @param arguments
+ * with the specified arguments.
+ */
 
-bool do_exec(int count, ...)
-{
-    va_list args;
-    va_start(args, count);
-    char * command[count+1];
-    int i;
-    for(i=0; i<count; i++)
-    {
-        command[i] = va_arg(args, char *);
+bool do_exec(int count, ...) {
+  bool result = false;
+  va_list args;
+  pid_t pid;
+  int status;
+
+  va_start(args, count);
+  char *command[count + 1];
+  for (int i = 0; i < count; i++) {
+    command[i] = va_arg(args, char *);
+    printf("Arg[%d]: %s\n", i, command[i]);
+  }
+  va_end(args);
+  command[count] = NULL;
+
+  pid = fork();
+  if (pid == -1) {
+    perror("Failed to fork process");
+    result = false;
+  } else if (pid == 0) {
+    printf("Executing %s\n", command[0]);
+    status = execv(command[0], command);
+    perror("Failed to execute command");
+    result = false;
+  } else {
+    printf("Forked process with pid %d\n", pid);
+    if (waitpid(pid, &status, 0) == -1) {
+      perror("Failed waiting on forked process");
+      result = false;
+    } else if (WIFEXITED(status)) {
+      printf("Process %s (%d) completed with status %d\n", command[0], pid,
+             WEXITSTATUS(status));
+      result = WEXITSTATUS(status) == 0;
+    } else {
+      perror("Forked process did not exit normally");
+      result = false;
     }
-    command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
-
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
-
-    va_end(args);
-
-    return true;
+  }
+  printf("do_exec result: %i\n", result);
+  return result;
 }
 
 /**
-* @param outputfile - The full path to the file to write with command output.
-*   This file will be closed at completion of the function call.
-* All other parameters, see do_exec above
-*/
-bool do_exec_redirect(const char *outputfile, int count, ...)
-{
-    va_list args;
-    va_start(args, count);
-    char * command[count+1];
-    int i;
-    for(i=0; i<count; i++)
-    {
-        command[i] = va_arg(args, char *);
+ * @param outputfile - The full path to the file to write with command output.
+ *   This file will be closed at completion of the function call.
+ * All other parameters, see do_exec above
+ */
+bool do_exec_redirect(const char *outputfile, int count, ...) {
+  bool result = false;
+  va_list args;
+  pid_t pid;
+  int status;
+  int fd;
+
+  va_start(args, count);
+  char *command[count + 1];
+  for (int i = 0; i < count; i++) {
+    command[i] = va_arg(args, char *);
+    printf("Arg[%d]: %s\n", i, command[i]);
+  }
+  va_end(args);
+  command[count] = NULL;
+
+  fd = open(outputfile, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+  if (fd < 0) {
+    perror("Failed to open file");
+    return false;
+  }
+
+  pid = fork();
+  if (pid == -1) {
+    perror("Failed to fork process");
+    result = false;
+  } else if (pid == 0) {
+    printf("Executing %s\n", command[0]);
+    if (dup2(fd, 1) < 0) {
+      perror("Failed to redirect stdout to file");
+    } else {
+      status = execv(command[0], command);
+      perror("Failed to execute command");
     }
-    command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
-
-
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
-
-    va_end(args);
-
-    return true;
+    result = false;
+  } else if (pid > 0) {
+    printf("Forked process with pid %d\n", pid);
+    if (waitpid(pid, &status, 0) == -1) {
+      perror("Failed waiting on forked process");
+      result = false;
+    } else if (WIFEXITED(status)) {
+      printf("Process %s (%d) completed with status %d\n", command[0], pid,
+             WEXITSTATUS(status));
+      result = WEXITSTATUS(status) == 0;
+    } else {
+      perror("Forked process did not exit normally");
+      result = false;
+    }
+  }
+  close(fd);
+  printf("do_exec_redirect result: %i\n", result);
+  return result;
 }
